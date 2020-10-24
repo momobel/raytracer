b76b0db0bafa126669603c79bf7c4283fee8786c
momobel <plopyomomo@gmail.com>
2020-10-25 00:30:59 +0200

    Move sphere into its own module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use structopt::StructOpt;
 mod image;
 mod ppm;
 mod ray;
+mod sphere;
 mod vec;
 
 #[derive(StructOpt, Debug)]
@@ -46,46 +47,7 @@ impl Viewport {
     }
 }
 
-#[derive(Debug)]
-struct Sphere {
-    pub center: vec::Point,
-    pub radius: f64,
-}
-
-fn hit_sphere(ray: &ray::Ray, sphere: &Sphere) -> Option<f64> {
-    // let S be a sphere of center C and radius r
-    // a point P is on the sphere if ||P - C||² = r²
-    // a vector V has ||V||² = V.V
-    // a ray R with origin O and direction D hits the sphere
-    // if for any t ||O + tD - C||² = r²
-    //               (O + tD - C).(O + tD - C) = r²
-    // which means t²||D||² + 2tD.(O - C) + ||O - C||² - r² = 0
-    // This is a quadratic equation with
-    // a = ||D||²
-    // b = 2D.(O-C)
-    // c = ||O - C||² - r²
-    // discriminant d is b² - 4ac
-    // if negative, no real solution exist so no intersection
-    // if 0, a single solution exists -b / 2a
-    // if positive, 2 solutions exist (-b +- sqrt(d)) / 2a
-    let c_to_o = ray.origin - sphere.center;
-    let a = ray.direction.length_squared();
-    // b has a factor 2 so let b = 2h
-    // the quadratic equation is t = (-b +- sqrt(b² - 4ac)) / 2a
-    // replacing b gives (-2h +- sqrt((2h)² - 4ac)) / 2a
-    // then              (-h +- sqrt(h² - ac)) / a
-    let half_b = vec::dot(&ray.direction, &c_to_o);
-    let c = c_to_o.length_squared() - sphere.radius * sphere.radius;
-    let discriminant = half_b * half_b - a * c;
-    if discriminant < 0.0 {
-        None
-    } else {
-        // return closest hit point since we know the sphere is ahead of the camera
-        Some((-half_b - discriminant.sqrt()) / a)
-    }
-}
-
-const SCENE_SPHERE: Sphere = Sphere {
+const SCENE_SPHERE: sphere::Sphere = sphere::Sphere {
     center: vec::Point {
         x: 0.0,
         y: 0.0,
@@ -95,7 +57,7 @@ const SCENE_SPHERE: Sphere = Sphere {
 };
 
 fn ray_color(ray: &ray::Ray) -> image::Color {
-    if let Some(t) = hit_sphere(ray, &SCENE_SPHERE) {
+    if let Some(t) = sphere::hit_sphere(ray, &SCENE_SPHERE) {
         let intersection = ray.at(t);
         let normal = intersection - SCENE_SPHERE.center;
         let normal = vec::unit(&normal);
--- src/ray.rs
+++ src/ray.rs
@@ -1,20 +1,20 @@
-use crate::vec;
+use crate::vec::{Point, Vector};
 
 #[derive(Debug)]
 pub struct Ray {
-    pub origin: vec::Point,
-    pub direction: vec::Vector,
+    pub origin: Point,
+    pub direction: Vector,
 }
 
 impl Ray {
-    pub fn new(origin: &vec::Point, direction: &vec::Vector) -> Ray {
+    pub fn new(origin: &Point, direction: &Vector) -> Ray {
         Ray {
             origin: *origin,
             direction: *direction,
         }
     }
 
-    pub fn at(&self, t: f64) -> vec::Point {
+    pub fn at(&self, t: f64) -> Point {
         self.origin + t * &self.direction
     }
 }
