1f782a5e7776a259ff85ddbf764910c77523c1af
momobel <plopyomomo@gmail.com>
2020-10-25 23:43:02 +0100

    Use diffuse material for spheres

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,12 @@ impl Camera {
     }
 }
 
+#[derive(Debug)]
+struct RenderSettings {
+    pub antialiasing_samples: u16,
+    pub ray_bounce_limit: u16,
+}
+
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let opt = Options::from_args();
@@ -86,26 +92,61 @@ fn main() {
         Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0),
     ]);
     // render
-    fill_image(&mut img, &camera, &world);
+    let settings = RenderSettings {
+        antialiasing_samples: 100,
+        ray_bounce_limit: 50,
+    };
+    fill_image(&mut img, &settings, &camera, &world);
     let file =
         fs::File::create(&opt.output).expect(format!("Failed to open {}", opt.output).as_str());
     let mut writer: ppm::PPMWriter<fs::File> = ppm::PPMWriter::new(file);
     writer.write(&img).expect("Failed to write image");
 }
 
-fn ray_color(ray: &Ray, world: &HittableVec<Sphere>) -> Color {
+fn random_vec(min: f64, max: f64) -> Vector {
+    let rand_range = rand::distributions::Uniform::new(min, max);
+    let mut rng = rand::thread_rng();
+    Vector::new(
+        rand_range.sample(&mut rng),
+        rand_range.sample(&mut rng),
+        rand_range.sample(&mut rng),
+    )
+}
+
+fn random_vec_in_unit_sphere() -> Vector {
+    let mut v: Vector;
+    loop {
+        v = random_vec(-1.0, 1.0);
+        if v.length_squared() < 1.0 {
+            break;
+        }
+    }
+    v
+}
+
+fn ray_color(ray: &Ray, world: &HittableVec<Sphere>, depth: i16) -> Color {
+    // ray bounced too many times, no more light is gathered
+    if depth < 0 {
+        return image::colors::BLACK;
+    }
     if let Some(hit) = world.hit_by(ray, 0.0, ray::T_INFINITY) {
-        return 0.5 * Color::new(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0);
+        let target = hit.point + hit.normal + random_vec_in_unit_sphere();
+        return 0.5 * ray_color(&Ray::new(hit.point, target - hit.point), world, depth - 1);
     }
     let unit_dir = vec::unit(&ray.direction);
     let t = 0.5 * (unit_dir.y + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn fill_image(img: &mut image::Image, camera: &Camera, world: &HittableVec<Sphere>) {
+fn fill_image(
+    img: &mut image::Image,
+    settings: &RenderSettings,
+    camera: &Camera,
+    world: &HittableVec<Sphere>,
+) {
     let range_rand = rand::distributions::Uniform::new(0.0, 1.0);
     let mut rng = rand::thread_rng();
-    let samples: u8 = 100;
+    let samples = settings.antialiasing_samples;
     for line in 0..img.height {
         eprint!("\rLines remaining: {:3}", img.height - line);
         io::stderr().flush().unwrap();
@@ -118,7 +159,7 @@ fn fill_image(img: &mut image::Image, camera: &Camera, world: &HittableVec<Spher
                 let v = (img.height as f64 - (line as f64 + range_rand.sample(&mut rng)))
                     / (img.height as f64 - 1.0);
                 let ray = camera.ray(u, v);
-                color = color + ray_color(&ray, world);
+                color = color + ray_color(&ray, world, settings.ray_bounce_limit as i16);
             }
             color = &color / samples as f64;
             color.clamp(0.0, 0.999);
